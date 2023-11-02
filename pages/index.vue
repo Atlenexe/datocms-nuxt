03bef6a85532b04fe72efaf91280ea0bacb69466
Atlenexe <hugo.mandou@gmail.com>
2023-11-02 15:32:25 +0100

    Apollo

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,8 @@
 <script setup>
-import { ref, reactive, onUpdated, computed } from 'vue';
-const count = ref(0);
-const state = reactive({ count: 0 });
-const src = 'https://picsum.photos/200/300';
-
-onUpdated(() => {
-    console.log(count.value);
-});
-
-function increment() {
-    count.value++;
-}
-
-function increment2() {
-    state.count++;
-}
-
-const user = computed(() => {
-    return 'John' + ' Doe';
-});
-
-let users = [
-    { name: 'John', age: 18 },
-    { name: 'Jane', age: 20 },
-    { name: 'Jack', age: 25 },
-];
-
+console.log('test');
 </script>
 
 <template>
-    <AppInfos>
-        <p>Contenu</p>
-    </AppInfos>
-
     <NuxtLink to="/page-1">
         Page 1
     </NuxtLink>
@@ -40,46 +10,6 @@ let users = [
     <a href="/page-1">Page 1</a>
 
     <PropComponent title="Titre" />
-
-    <button @click="increment()">
-        {{ count }} fois
-    </button>
-
-    <button @click="increment2()" v-if="state.count <= 5">
-        {{ state.count }} fois
-    </button>
-
-    <span v-else>
-        Stop
-    </span>
-
-    <span>
-        {{ user }}
-    </span>
-
-    <br>
-
-    <span v-for="i in count">
-        {{ i }} fois
-    </span>
-
-    <br>
-
-    <span v-for="user in users">
-        {{ user.name }} a {{ user.age }} ans
-    </span>
-
-    <br>
-
-    <img src="~assets/dog.png" alt="">
-
-    <img :src="src" alt="">
 </template>
 
-<style scoped>
-button {
-    color: white;
-    background-color: black;
-    font-weight: bold;
-}
-</style>
+<style scoped></style>
--- pages/page-1.vue
+++ pages/page-1.vue
@@ -1,17 +1,8 @@
 <script setup>
-import { useCounterStore } from '@/store/counter';
-import { storeToRefs } from 'pinia';
 const { data, pending, error, refresh } = await useFetch('https://api.nuxtjs.dev/mountains');
-
-const counterStore = useCounterStore();
-
-const { increment } = counterStore;
-const { counter } = storeToRefs(counterStore);
 </script>
 
 <template>
-    <button @click="increment()">Increment</button>
-    <p>{{ counter }}</p>
     <span v-if="pending">Loading...</span>
     <div v-else v-for="(mountain, index) in data" :key="index">
         <h2>{{ mountain.title }}</h2>
